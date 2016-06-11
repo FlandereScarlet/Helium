b729326fe6444f4d8567f50b84bd014fe8965e53
Zachary <zacimac@users.noreply.github.com>
2016-06-11 14:34:10 +1000

    Added run.bat file

---
+++ run.bat
@@ -0,0 +1,10 @@
+cd C:\Users\zacwi\Documents\Programing\Discord Bot\Helium\botdir
+title Helium Console
+color 0D
+@echo off
+@echo off CLS
+cls
+echo Launching Helium...
+timeout /t 10 /nobreak
+cls
+node Helium.js
