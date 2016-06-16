bac35d23b272b44a14aa9e6a8c42d0b818244807
Chearful <chearful@chearfulcraft.org>
2016-06-16 18:30:15 +0100

    Adjusted some things.
    The script would only be able to change directory if there is a user named zacwi. There is also no need for the timeout.

--- run.bat
+++ run.bat
@@ -1,10 +1,7 @@
-cd C:\Users\zacwi\Documents\Programing\Discord Bot\Helium\botdir
 title Helium Console
 color 0D
 @echo off
-@echo off CLS
 cls
 echo Launching Helium...
-timeout /t 10 /nobreak
 cls
 node Helium.js
